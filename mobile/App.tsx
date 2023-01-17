4ac76dad6ddf3cd8ac9d461f656c295a4e74d715
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-01-17 20:53:31 -0300

    Finished Stage 1

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold,
+  useFonts,
+} from "@expo-google-fonts/inter";
+import { StatusBar, StyleSheet, Text, View } from "react-native";
+import { Loading } from "./src/components/Loading";
 
 export default function App() {
+  const [fontsLoading] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold,
+  });
+
+  if (!fontsLoading) {
+    return <Loading />;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.font}>
+        Open up App.tsx to start working on your app!
+      </Text>
+      <StatusBar
+        translucent
+        barStyle="light-content"
+        backgroundColor="transparent"
+      />
     </View>
   );
 }
@@ -13,8 +37,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#09090A",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  font: {
+    color: "white",
+    textAlign: "center",
+    fontFamily: "Inter_400Regular",
+    fontSize: 24,
   },
 });
