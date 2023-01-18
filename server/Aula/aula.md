03e38202a7b64e3cba03e3abd6dc33e6c6fa0239
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-01-17 23:36:07 -0300

    Configurando cors

---
+++ server/Aula/aula.md
@@ -0,0 +1,16 @@
+# Guia Nlw Aula 01
+
+## Back-end
+
+- [] Fundamentos do Nodejs
+- [] Criação do projeto
+- [] Setup Typescript
+- [] Configurando Prisma
+- [] Configurando Cors
+
+## Front-end
+
+- [] Fundamentos do React
+- [] Criação do projeto
+- [] Componentes e propriedades
+- [] Configurando Tailwind CSS
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,14 @@
+import cors from "@fastify/cors";
+import { PrismaClient } from "@prisma/client";
 import Fastify from "fastify";
 
 const app = Fastify();
+const prisma = new PrismaClient();
+app.register(cors);
 
-app.get("/", (req, res) => {
-  return "Hello World!";
+app.get("/", async (req, res) => {
+  const habits = await prisma.habit.findMany();
+  return habits;
 });
 
 app
