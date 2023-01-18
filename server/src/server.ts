22cd333dfd54605e2e5e1654015d22b8eaf530b9
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-01-17 23:18:08 -0300

    Install Prisma ORN

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,13 @@
-console.log("oi");
+import Fastify from "fastify";
+
+const app = Fastify();
+
+app.get("/", (req, res) => {
+  return "Hello World!";
+});
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => console.log("HTTP server running on port 3333"));
