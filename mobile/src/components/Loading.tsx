e36613817f47d9fae6d5c2d4d8a2cb2a33a8ed63
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-01-18 21:41:14 -0300

    Delete mobile directory

--- mobile/src/components/Loading.tsx
+++
@@ -1,9 +0,0 @@
-import { ActivityIndicator, View } from "react-native";
-
-export function Loading() {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <ActivityIndicator color="#7C3AED" />
-    </View>
-  );
-}
