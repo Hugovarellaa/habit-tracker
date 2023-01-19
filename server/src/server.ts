20295f8430e26eb70fe896c73080d336be2b47be
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-01-18 22:26:20 -0300

    cors fastify

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,10 @@
+import cors from "@fastify/cors";
 import { PrismaClient } from "@prisma/client";
 import Fastify from "fastify";
 
 const app = Fastify();
 const prisma = new PrismaClient();
+app.register(cors);
 
 app.get("/", (req, res) => {
   const habit = prisma.habit.findMany();
