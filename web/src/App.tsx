235f54f560d6648497de24a8f30bd0cd0766a7d7
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-01-19 14:05:51 -0300

    Component Header

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,13 @@
 import { Habit } from "./components/Habit";
+import { Header } from "./components/Header";
+import "./styles/main.css";
 
 export function App() {
   return (
-    <div>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header />
+      </div>
       <Habit />
     </div>
   );
